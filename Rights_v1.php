7af98b33f259a06e8d4a04ee9be774a5b75a3885
Louis Kuznik <louis.kuznik@gmail.com>
2018-05-10 22:30:16 +0200

    1er jet de la page gestion des droits utilisateur

---
+++ Rights_v1.php
@@ -0,0 +1,131 @@
+    <html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width"/>
+    <title>Supervision du panneau</title>
+    <link rel="stylesheet" media="screen" type="text/css" href=".\Style.css" /> 
+</head>
+
+<body> 
+
+ <div class="Bground_grid">
+  <div class="Bground_center">
+  </div>
+ </div>
+ 
+
+ 
+ 
+ 
+<div class="back_r">
+ <div class="page_title">Gestion des droits
+ </div>
+ <div class="search_menu">
+   <form>
+     <div id="search_user" class="search-user">
+       <input class="">
+       <a id="search_button" class="btn search-button">Search</a>
+     </div>
+     <div>
+       <div>
+         <select id="search_grp">
+           <option value="0">[Pas de sélection]</option>
+           <option value="1">sti2d</option>
+           <option value="2">electrotech</option>
+           <option value="3">/</option>
+         </select>
+       </div>
+       <div>
+         <select name="date" id="search_month">
+           <option value="2018.05">Mai 2018</option>
+           <option value="2018.04">Avr 2018</option>
+           <option value="2018.03">Mar 2018</option>
+           <option value="2018.02">Fev 2018</option>
+           <option value="2018.01">Jan 2018</option>
+           <option value="2017.12">Dec 2017</option>
+           <option value="2017.11">Nov 2017</option>
+         </select>
+       </div>
+     </div>
+   </form>
+ </div>
+ <div class="content-table menu-table">
+   <div class="table-header">
+     <div class="first"> Nom d'utilisateur </div>
+     <div class="second"> Créé le </div>
+     <div class="third"> On/off ?</div>
+     <div class="fourth"> Groupe </div>
+   </div>
+   <div>
+     <div class="user-content">
+       <div class="name-user first">
+         <div> l.kuznik </div>
+       </div>
+       <div class="second lato">2018-01-31</div>
+       <div class="third lato">
+         <div class="checkbox-power">
+	       <input type="checkbox" value="None" id="power1" name="check" />
+	       <label for="power1"></label>
+         </div>
+       </div>
+       <div class="fourth lato">
+         <div class="active">
+                <select id="grp_user">
+                    <option value="0">/</option>
+                    <option value="1">sti2d</option>
+                    <option value="2">electrotech</option>
+                </select>
+         </div>
+       </div>
+     </div>
+   </div>
+   <div>
+     <div class="user-content">
+       <div class="name-user first">
+         <div> m.aissaoui </div>
+       </div>
+       <div class="second lato">2018-01-31</div>
+       <div class="third lato">
+         <div class="checkbox-power">
+	       <input type="checkbox" value="None" id="power1" name="check" />
+	       <label for="power1"></label>
+         </div>
+       </div>
+       <div class="fourth lato">
+         <div class="active">
+                <select id="grp_user">
+                    <option value="0">/</option>
+                    <option value="1">sti2d</option>
+                    <option value="2">electrotech</option>
+                </select>
+         </div>
+       </div>
+     </div>
+   </div>
+   <div>
+     <div class="user-content">
+       <div class="name-user first">
+         <div> j.destruy </div>
+       </div>
+       <div class="second lato">2018-01-31</div>
+       <div class="third lato">
+         <div class="checkbox-power">
+	       <input type="checkbox" value="None" id="power1" name="check" />
+	       <label for="power1"></label>
+         </div>
+       </div>
+       <div class="fourth lato">
+         <div class="active">
+                <select id="grp_user">
+                    <option value="0">/</option>
+                    <option value="1">sti2d</option>
+                    <option value="2">electrotech</option>
+                </select>
+         </div>
+       </div>
+     </div>
+   </div>
+ </div>
+</div>       
+</body>
+     </html>
