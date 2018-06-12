bcacf10ce13b233e1889ab11193bf05b958bd0f9
Louis Kuznik <louis.kuznik@gmail.com>
2018-06-12 12:39:50 +0200

    fichier php focntion delete

---
+++ Rights_v1.4/delete.php
@@ -0,0 +1,20 @@
+<?php
+
+$user = 'root';
+$pass = 'admin';
+
+try{
+    
+    $bdd = new PDO('mysql:host=localhost;port=3304;dbname=espace_membres;charset=utf8', $user, $pass);
+}
+catch(Exception $e) {
+    die('Erreur : '.$e->getMessage());
+}
+
+    $delete = $bdd->query('SELECT id FROM membres');
+    $fordelete = $delete->fetch();
+    $iddelete = (!empty($_GET['id']))? intval($_GET['id']) : 0;
+    $bdd->exec("DELETE FROM membres WHERE id='$iddelete'");
+    header('Location:rights.php');
+
+?>
